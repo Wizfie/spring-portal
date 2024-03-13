639c6875f049815f3b9241e81141c047bf5f6144
Wisfie <wiz.fie@gmail.com>
2024-03-13 16:42:53 +0700

    download file oke

--- src/main/java/com/ms/springms/controller/FileController.java
+++ src/main/java/com/ms/springms/controller/FileController.java
@@ -5,16 +5,20 @@ import com.ms.springms.entity.EventStages;
 import com.ms.springms.entity.Registration;
 import com.ms.springms.service.upload.UploadFilesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 
 @RestController
 @RequestMapping("/api/file")
-public class UploadFileController {
+public class FileController {
 
     @Autowired
     private UploadFilesService uploadFilesService;
@@ -33,6 +37,24 @@ public class UploadFileController {
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload file: " + e.getMessage());
         }
+    }
+
+    @GetMapping("/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName) throws MalformedURLException {
+        Resource resource = uploadFilesService.downloadFile(fileName);
+
+        String contentType = "application/octet-stream";
+        try{
+            contentType = resource.getURL().openConnection().getContentType();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return  ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION,"attachment; fileName=\"" + resource.getFilename()+ "\"")
+                .body(resource);
     }
 
 }
--- src/main/java/com/ms/springms/service/upload/UploadFilesService.java
+++ src/main/java/com/ms/springms/service/upload/UploadFilesService.java
@@ -5,10 +5,14 @@ import com.ms.springms.entity.Registration;
 import com.ms.springms.entity.UploadFiles;
 import com.ms.springms.repository.UploadFileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -22,6 +26,22 @@ public class UploadFilesService {
     @Autowired
     private UploadFileRepository uploadFileRepository;
 
+    private final Path fileStorageLocation;
+
+    private final String uploadDir = "./uploads/" ;
+
+
+    public UploadFilesService() {
+        this.fileStorageLocation = Paths.get(uploadDir).toAbsolutePath().normalize();
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (IOException ex) {
+            throw new RuntimeException("Failed to create directory for uploaded files", ex);
+        }
+    }
+
+
+
 
     public void uploadFile(MultipartFile file ,String eventName , String teamName ,EventStages eventStages, Registration registration) throws IOException {
 
@@ -34,7 +54,6 @@ public class UploadFilesService {
         try {
             String fileName = file.getOriginalFilename();
             String uniqueFileName = generateUniqueFileName(fileName, eventName , teamName);
-            String uploadDir = "./uploads/" + eventName + "/" + teamName + "/";
             Path uploadPath = Paths.get(uploadDir);
 
             // create folder
@@ -66,6 +85,18 @@ public class UploadFilesService {
     public List<UploadFiles> getALl(){
         return uploadFileRepository.findAll();
     }
+
+
+    public Resource downloadFile(String fileName) throws MalformedURLException {
+        Path  filePath = this.fileStorageLocation.resolve(fileName).normalize();
+        Resource resource = new UrlResource(filePath.toUri());
+        if (resource.exists()){
+            return resource;
+        } else {
+                throw new RuntimeException("File no found " + fileName);
+        }
+    }
+
 
     private String generateUniqueFileName(String originalFileName, String eventName, String teamName) {
         String fileNameWithoutExtension = originalFileName.substring(0, originalFileName.lastIndexOf('.'));
